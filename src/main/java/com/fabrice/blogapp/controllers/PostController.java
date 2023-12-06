b5f613fb4e2970b8f446c27b4d947717aacaec54
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-06 16:25:32 +0200

    getting single post

--- src/main/java/com/fabrice/blogapp/controllers/PostController.java
+++ src/main/java/com/fabrice/blogapp/controllers/PostController.java
@@ -1,10 +1,13 @@
 package com.fabrice.blogapp.controllers;
 
+import com.fabrice.blogapp.exceptions.ResourceNotFoundException;
 import com.fabrice.blogapp.models.Post;
 import com.fabrice.blogapp.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -19,5 +22,19 @@ public class PostController {
     @ResponseStatus(HttpStatus.CREATED)
     public Post addNewPost(@RequestBody Post newPost){
         return postRepository.save(newPost);
+    }
+
+    @GetMapping("")
+    public List<Post> getAllPosts(){
+        List<Post> posts = postRepository.findAll();
+        return posts;
+    }
+
+    //getting single post
+    @GetMapping("/{id}")
+//    public Post getPost(@PathVariable long id){    => because our variable is the same as the path variable
+    public Post getPost(@PathVariable(name = "id") long postId){ //here variable names are different that is why we have to specify the name of the pathvariable
+        Post post  = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("post"));
+        return post;
     }
 }
