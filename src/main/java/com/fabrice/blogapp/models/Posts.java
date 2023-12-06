dd12cc5bd1be58c124ae70ceb139f2b0976f22c2
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-06 11:29:35 +0200

    created posts table

--- src/main/java/com/fabrice/blogapp/models/Posts.java
+++ src/main/java/com/fabrice/blogapp/models/Posts.java
@@ -1,10 +1,55 @@
 package com.fabrice.blogapp.models;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 
 public class Posts {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false,length = 200)
     private  String title;
     private String description;
     private String content;
+
+
+    //getters & setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
